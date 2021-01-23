fd3a57f9ccb1554e741c9c8800e63cae929b7083
CiMiKo <private.hartono@mail.com>
2021-01-23 09:44:37 +0700

    Menambahkan Navbar

---
+++ atomic-design/src/templates/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavHead from '../../Components/Navbar'
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <NavHead/>
+         );
+    }
+}
+ 
+export default Header;
