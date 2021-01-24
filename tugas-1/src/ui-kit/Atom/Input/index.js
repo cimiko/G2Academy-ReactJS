673d7efb5a536bd73b80ff1498c1475d3bacf04f
CiMiKo <private.hartono@mail.com>
2021-01-24 19:38:22 +0700

    Command Scirpt yang tak perlu

--- tugas-1/src/ui-kit/Atom/Input/index.js
+++ tugas-1/src/ui-kit/Atom/Input/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Input = ({ type, name, placeholder, className, value, min, max}) => {
+const Input = ({ type, name, placeholder, className, value, min, max, onChange}) => {
     return (
-        <input type={type} name={name} value={value} placeholder={placeholder} className={className} min={min} max={max} />
+        <input type={type} name={name} value={value} placeholder={placeholder} className={className} min={min} max={max} onChange={onChange} />
     )
 }
 
--- tugas-1/src/ui-kit/component/ListPeople/index.js
+++ tugas-1/src/ui-kit/component/ListPeople/index.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Input, Button } from 'ui-kit/Atom';
+import style from './style.module.css';
+import { Table } from 'react-bootstrap'
 
-function Paragraph() {
-    return <p>Halo</p>
-}
+// function Paragraph() {
+//     return <p>Halo</p>
+// }
 
 class ListPeople extends Component {
     constructor() {
@@ -42,16 +45,17 @@ class ListPeople extends Component {
     }
 
     onInput = e => {
-        this.setState({ name: e.target.value})
+        this.setState({ name: e.target.value })
         console.log(this.state.name);
     }
 
     addPeople = (e) => {
         e.preventDefault()
-        let joined = this.state.people.concat( {
+        let joined = this.state.people.concat({
             id: this.state.people.length + 1,
-            name: this.state.name, 
-            status: "Tambahan Baru" } );
+            name: this.state.name,
+            status: "Tambahan Baru"
+        });
         this.setState({ people: joined, name: '' })
         console.log(this.state.people);
     }
@@ -61,14 +65,14 @@ class ListPeople extends Component {
 
         return (
             <>
+                <div className={style.flex}>
+                    <Input type="text" value={this.state.name} placeholder="Masukan Nama Anda!!" onChange={this.onInput} className="form-control w-25" />
+                    <Button className={style.btn} onClick={this.addPeople}>Add</Button>
+                </div>
                 <People
                     people={people}
-                    myFunction={this.myFunction}
-                    Paragraph={<Paragraph />}
                     {...restState}
                 />
-                <input type="text" value={this.state.name} placeholder="Masukan Nama Anda!!" onChange={this.onInput}/>
-                <button onClick={this.addPeople}>Add</button>
             </>
         )
     }
@@ -83,10 +87,10 @@ function People({
     myFunction,
     Paragraph
 }) {
-    const onClick = name => {
-        console.log(name)
-        myFunction()
-    }
+    // const onClick = name => {
+    //     console.log(name)
+    //     myFunction()
+    // }
 
     // const addPeople = () => {
     //     // e.preventDefault()
@@ -96,20 +100,34 @@ function People({
 
     return (
         <>
-            <p>name: {name}</p>
-            <p>room: {room}</p>
-            <p>is bootcamp: {is_bootcamp ? 'iya' : 'bukan'}</p>
-            <p>session: {session[2]}</p>
-            {Paragraph}
+            {/* <P>name: {name}</P>
+            <P>room: {room}</P>
+            <P>is bootcamp: {is_bootcamp ? 'iya' : 'bukan'}</P>
+            <P>session: {session[2]}</P>
+            {Paragraph} */}
             {/* <input type="text" name="name" value={name} placeholder="input nama jedi baru" onChange={onChange()}/> */}
             {/* <button>Add Jedi</button> */}
-            <ul>
-                {people.map(x => (
-                    <li key={x.id} onClick={() => onClick(x.name)}>
-                        {x.name}
-                    </li>
-                ))}
-            </ul>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nama</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {people.map(x => (
+                        // <li key={x.id} onClick={() => onClick(x.name)}>
+                        //     {x.name}
+                        // </li>
+                        <tr>
+                            <td>{x.id}</td>
+                            <td>{x.name}</td>
+                            <td>{x.status}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
