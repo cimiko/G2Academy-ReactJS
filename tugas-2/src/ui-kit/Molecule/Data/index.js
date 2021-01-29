98843a22aa5260ffdccd3aeca7fa15126b513b5a
CiMiKo <private.hartono@mail.com>
2021-01-29 19:56:31 +0700

    Menambahkan folder tugas3

--- tugas-2/src/ui-kit/Molecule/Data/index.js
+++ tugas-2/src/ui-kit/Molecule/Data/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap'
+import { Table, Pagination } from 'react-bootstrap'
 import PropTypes from 'prop-types';
-import {NavBar} from 'ui-kit/Molecule'
+import { NavBar } from 'ui-kit/Molecule'
 import Style from './style.module.css'
 
 class Data extends Component {
@@ -10,28 +10,18 @@ class Data extends Component {
         this.state = {
             search: '',
             filter: '',
-            person: [{
-                id: 19918656,
-                nama: "Andi",
-                domisili: "Jakarta"
-            }, {
-                id: 19935874,
-                nama: "Joko",
-                domisili: "Bekasi"
-            }, {
-                id: 19964852,
-                nama: "Regina",
-                domisili: "Bogor"
-            }, {
-                id: 19729875,
-                nama: "Saiful",
-                domisili: "Tangerang"
-            }, {
-                id: 19855698,
-                nama: "Rudi",
-                domisili: "Serang"
-            }]
+            page: 2,
+            person: []
         }
+    }
+
+    componentDidMount() {
+        fetch(`https://swapi.dev/api/people/?page=${this.state.page}`)
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                this.setState({ person: res.results })
+            })
     }
 
     onInput = e => {
@@ -46,13 +36,33 @@ class Data extends Component {
         this.setState({ filter: filterData })
     }
 
+    onPrev = () => {
+        this.setState({ page: this.state.page - 1})
+        console.log(this.state.page);
+    }
+
+    onNext = () => {
+        // this.setState({page: this.state.page + 1})
+        // console.log(this.state.page);
+        fetch(`https://swapi.dev/api/people/?page=2`)
+            .then(res => res.json())
+            .then(res => res.next())
+            .then(res => {
+                console.log(res)
+                this.setState({ person: res.results })
+            })
+    }
+
     render() {
-        const { person, filter } = this.state
+        const { person, filter, page } = this.state
         const fill = filter !== '' ? filter : person
         return (
             <>
                 <NavBar search={this.onInput} btnSearch={this.onSearch} />
-                <PersonList person={fill} />
+                <div className={Style.listData}>
+                    <PersonList person={fill} />
+                    <Page className={`justify-content-center ${Style.Page}`} page={page} onPrev={this.onPrev} onNext={this.onNext} />
+                </div>
             </>
         );
     }
@@ -65,23 +75,36 @@ const PersonList = ({ person }) => {
                 <thead>
                     <tr>
                         <th>No</th>
-                        <th>ID Person</th>
                         <th>Nama</th>
-                        <th>Domisili</th>
+                        <th>Gender</th>
                     </tr>
                 </thead>
                 <tbody>
                     {person.map((x, index) =>
-                        <tr key={x.id}>
+                        <tr key={index}>
                             <td>{index + 1}</td>
-                            <td>{x.id}</td>
-                            <td>{x.nama}</td>
-                            <td>{x.domisili}</td>
+                            <td>{x.name}</td>
+                            <td>{x.gender}</td>
                         </tr>
                     )}
                 </tbody>
             </Table>
         </div>
+    )
+}
+const Page = ({ className, page, onPrev, onNext }) => {
+    return (
+        <Pagination className={className}>
+            <Pagination.First />
+            <Pagination.Prev />
+
+            <Pagination.Item onClick={onPrev}>{page - 1}</Pagination.Item>
+            <Pagination.Item active>{page}</Pagination.Item>
+            <Pagination.Item onClick={onNext}>{page + 1}</Pagination.Item>
+
+            <Pagination.Next />
+            <Pagination.Last />
+        </Pagination>
     )
 }
 
