308ab2625501f70a07aeaebe203c19f7a3efa721
CiMiKo <private.hartono@mail.com>
2021-02-04 00:32:06 +0700

    Menghapus yang tidak perlu

--- tugas4/src/App.js
+++ tugas4/src/App.js
@@ -10,21 +10,19 @@ import {
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/destinasi">
-            <Destinasi />
-          </Route>
-          <Route path="/promo">
-            <Promo />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/destinasi">
+          <Destinasi />
+        </Route>
+        <Route path="/promo">
+          <Promo />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- tugas4/src/pages/Destinasi/index.js
+++ tugas4/src/pages/Destinasi/index.js
@@ -103,6 +103,7 @@ function Destinasi() {
                     </Body>
                 </Route>
             </Switch>
+            
             <Footer />
         </div>
     )
